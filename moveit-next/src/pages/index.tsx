55196785ef10df30f7ba6e3f48c86897e2cb42c2
kaiquesilva1 <kaiquesilvar2020@gmail.com>
2021-02-23 14:12:13 -0300

    criando o component CountDown

--- moveit-next/src/pages/index.tsx
+++ moveit-next/src/pages/index.tsx
@@ -1,16 +1,25 @@
 import {ExperienceBar} from "../components/ExperienceBar";
 import {Profile} from "../components/Profile"
+import {CompletedChallenges} from "../components/CompletedChallenges";
+import {CountDown} from "../components/CountDown";
+
+import Head from "next/head";
 
 import style from '../style/pages/Home.module.css'
 
 export default function Home() {
   return (
       <div className={style.container}>
+          <Head>
+              <title>Início | move.it</title>
+          </Head>
         <ExperienceBar />
 
         <section>
             <div>
                 <Profile />
+                <CompletedChallenges />
+                <CountDown />
             </div>
             <div></div>
         </section>
