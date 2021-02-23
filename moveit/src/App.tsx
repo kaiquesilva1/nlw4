76607172c44f946832b99fb22e44dd46c99bb0d1
kaiquesilva1 <kaiquesilvar2020@gmail.com>
2021-02-22 22:31:41 -0300

    fazendo o primeiro componente ExperienceBar

---
+++ moveit/src/App.tsx
@@ -0,0 +1,12 @@
+import './style/global.css'
+import {ExperienceBar} from './components/ExperienceBar'
+
+function App() {
+  return (
+      <div className={'container'}>
+        <ExperienceBar />
+      </div>
+  );
+}
+
+export default App;
